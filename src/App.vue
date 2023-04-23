28d449ae0e3869eb35346b5919ae6811a5ff6e4d
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-04-24 04:34:04 +0600

    Included WeatherLayout.

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import WeatherLayout from '@/features/weather/layouts/WeatherLayout.vue'
+</script>
+
+<template>
+  <WeatherLayout />
+</template>
+
+<style scoped></style>
