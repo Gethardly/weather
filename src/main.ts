36de5a6323655d3d4d086737a3ea2b372af51e23
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-10-09 06:28:19 +0600

    Added localization.

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,23 @@ import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
 import App from './App.vue'
 import store from './store/store'
+import { createI18n } from 'vue-i18n'
+import english from './locales/en.json'
+import russian from './locales/ru.json'
+import 'dayjs/locale/ru'
 
 const app = createApp(App)
+
+const i18n = createI18n({
+  locale: 'ru',
+  fallbackLocale: 'ru',
+  messages: {
+    en: english,
+    ru: russian
+  }
+})
 app.use(store)
+app.use(i18n)
 app.use(Quasar, {
   config: {},
   plugins: {}
