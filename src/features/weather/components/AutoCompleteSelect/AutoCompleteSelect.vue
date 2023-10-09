b8de90f142393039ebc6fc604195aedb526c6091
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2023-10-09 06:25:39 +0600

    Added props for the parent component also added api for the cities.

--- src/features/weather/components/AutoCompleteSelect/AutoCompleteSelect.vue
+++ src/features/weather/components/AutoCompleteSelect/AutoCompleteSelect.vue
@@ -1,76 +1,80 @@
 <template>
-  <q-form style="width: 500px" class="row">
+  <q-form class="row">
     <q-card-section>
       <q-select
-        filled
-        bg-color="white"
-        label="Search a city"
-        v-model="city"
-        use-input
-        hide-selected
-        fill-input
-        clearable
-        input-debounce="0"
-        :loading="loading"
-        :options="options"
-        @filter="filterFn"
-        style="width: 250px"
+          filled
+          bg-color="white"
+          v-model="city"
+          :label="$t('searchCity')"
+          use-input
+          hide-selected
+          fill-input
+          clearable
+          input-debounce="0"
+          :loading="loading"
+          :options="options"
+          @filter="filterFn"
+          style="width: 250px"
       >
         <template v-slot:no-option>
           <q-item>
-            <q-item-section class="text-grey"> No results </q-item-section>
+            <q-item-section class="text-grey"> No results</q-item-section>
           </q-item>
         </template>
       </q-select>
     </q-card-section>
     <q-card-actions>
-      <q-toggle v-model="toggle" :label="units" :disable="!city" style="color: white" />
+      <q-toggle v-model="toggle" :label="$t(units === 'Metric' ? 'metric' : 'imperial')" :disable="!city" style="color: white"/>
     </q-card-actions>
   </q-form>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import citiesFile from '../../../../assets/city.list.json'
-import type { City } from '@/types'
-import { Units } from '@/features/weather/layouts/WeatherLayout.vue'
+import axios from 'axios';
+import type { CityName } from '@/types';
 
-const citiesString = JSON.stringify(citiesFile)
-const citiesArray: City[] = JSON.parse(citiesString)
-
-const stringOptions: string[] = citiesArray.map((city) => {
-  return city.name
-})
+const cities = ['Bishkek', 'Moscow', 'Dubai', 'NY', 'Kant']
 
 export default defineComponent({
   name: 'AutoCompleteSelect',
   data() {
     return {
-      city: '',
-      units: Units.Metric,
+      city: this.$props.selectedCity,
+      units: 'Metric',
       toggle: false,
-      options: stringOptions
+      options: cities,
+      loading: false
     }
   },
   props: {
-    loading: Boolean
+    selectedCity: String,
   },
   watch: {
-    city(value: City) {
+    city(value) {
       if (value) {
         this.$emit('cityChanged', this.city)
       }
     },
     toggle(value: boolean) {
-      this.units = value ? Units.Imperial : Units.Metric
+      this.units = value ? 'Imperial' : 'Metric'
       this.$emit('toggleUnits', this.units)
     }
   },
   methods: {
-    filterFn(val: string, update: Function) {
-      update(() => {
+    async filterFn(val: string, update: Function) {
+      this.loading = true; // Устанавливаем флаг загрузки
+      update(async () => {
         const needle = val.toLowerCase()
-        this.options = stringOptions.filter((v) => v.toLowerCase().indexOf(needle) > -1)
+        const response = await axios.get(`http://api.geonames.org/searchJSON`, {
+          params: {
+            name_startsWith: needle,
+            maxRows: 100,
+            username: 'tuxbary'
+          }
+        })
+        this.options = response.data.geonames.map((item: CityName) => item.name)
+        this.loading = false; // Снимаем флаг загрузки после получения данных
       })
     }
   }
